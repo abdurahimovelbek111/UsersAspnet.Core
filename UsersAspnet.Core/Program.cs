92dd74a8bea4c970dcb54dcaf0d35ef9fb58202f
Elbek Abdurahimov <abdurahimovelbek111@gmail.com>
2022-04-06 14:51:37 +0500

    gfvggg

--- UsersAspnet.Core/Program.cs
+++ UsersAspnet.Core/Program.cs
@@ -15,6 +15,12 @@ namespace UsersAspnet.Core
         {
             CreateHostBuilder(args).Build().Run();
         }
+
+
+
+
+
+
        public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
